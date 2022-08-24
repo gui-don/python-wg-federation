9c9c4424587b21294a23d32e3955db450c9a6d1c
Guillaume DONVAL <guillaume.donval@globaleagle.com>
2022-08-24 13:05:46 -0400

    feat: adds base hello-world code

---
+++ src/wg_federation/__init__.py
@@ -0,0 +1,9 @@
+""" wg-federation """
+
+import hello
+from constants import __version__
+
+
+def main():
+    """ Main """
+    hello.hello(__version__)
---
+++ src/wg_federation/hello.py
@@ -0,0 +1,6 @@
+""" Hello World dummy code """
+
+
+def hello(version: str):
+    """ Says hello. """
+    print('Hello! from version '+version)
