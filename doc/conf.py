bf8a4ffb655ef1c491b42f08e2d6a79281de628d
Guillaume DONVAL <guillaume.donval@globaleagle.com>
2022-09-07 12:39:37 -0400

    refactor: cleans conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -52,12 +52,7 @@ extensions = [
     'sphinx.ext.napoleon',
 ]
 
-
 templates_path = ['_templates']
-
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
