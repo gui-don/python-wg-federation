3ebc523ab373effd226620ef6ba00cabb50a8538
Guillaume DONVAL <guillaume.donval@globaleagle.com>
2022-10-12 12:30:04 -0400

    test: renames _controllerX pytest class attributes to better named attributes

--- tests/unit/wg_federation/controller/dispatcher/test_controller_dispatcher.py
+++ tests/unit/wg_federation/controller/dispatcher/test_controller_dispatcher.py
@@ -10,10 +10,10 @@ from wg_federation.controller.dispatcher.controller_status import Status
 class TestControllerDispatcher:
     """ Test ControllerDispatcher class """
 
-    _controller1 = MagicMock()
-    _controller2 = MagicMock()
-    _controller3 = MagicMock()
-    _controller4 = MagicMock()
+    _working_controller = MagicMock()
+    _not_run_controller = MagicMock()
+    _working_controller_controller2 = MagicMock()
+    _error_raised_controller = MagicMock()
     _logger = MagicMock()
     _user_input = MagicMock()
 
@@ -27,19 +27,19 @@ class TestControllerDispatcher:
     def setup(self):
         """ Constructor """
 
-        self._controller1.should_run = MagicMock(return_value=True)
-        self._controller1.run = MagicMock(return_value=Status.SUCCESS)
+        self._working_controller.should_run = MagicMock(return_value=True)
+        self._working_controller.run = MagicMock(return_value=Status.SUCCESS)
 
-        self._controller2.should_run = MagicMock(return_value=False)
+        self._not_run_controller.should_run = MagicMock(return_value=False)
 
-        self._controller3.should_run = MagicMock(return_value=True)
-        self._controller3.run = MagicMock(return_value=Status.SUCCESS)
+        self._working_controller_controller2.should_run = MagicMock(return_value=True)
+        self._working_controller_controller2.run = MagicMock(return_value=Status.SUCCESS)
 
-        self._controller4.run = MagicMock(return_value=Status.DEFAULT_ERROR)
+        self._error_raised_controller.run = MagicMock(return_value=Status.DEFAULT_ERROR)
 
         self._subject = ControllerDispatcher(
             controllers=[
-                self._controller1, self._controller2, self._controller3
+                self._working_controller, self._not_run_controller, self._working_controller_controller2
             ],
             logger=self._logger
         )
@@ -50,26 +50,26 @@ class TestControllerDispatcher:
 
     def test_enroll1(self):
         """ it enrolls new controllers """
-        self._subject.enroll(self._controller4)
+        self._subject.enroll(self._error_raised_controller)
 
     def test_enroll2(self):
         """ it raises an exception when trying to enroll an already-enrolled controller """
         with pytest.raises(RuntimeError):
-            self._subject.enroll(self._controller3)
+            self._subject.enroll(self._working_controller_controller2)
 
     def test_dispatch_all1(self):
         """ it dispatches all the controllers in order """
         self._subject.dispatch_all(self._user_input)
 
-        self._controller1.run.assert_called_once_with(self._user_input)
-        self._controller2.run.assert_not_called()
-        self._controller3.run.assert_called_once_with(self._user_input)
+        self._working_controller.run.assert_called_once_with(self._user_input)
+        self._not_run_controller.run.assert_not_called()
+        self._working_controller_controller2.run.assert_called_once_with(self._user_input)
 
     def test_dispatch_all2(self):
         """ it raises an Exception when one of the controller fails """
-        self._subject.enroll(self._controller4)
+        self._subject.enroll(self._error_raised_controller)
 
         with pytest.raises(RuntimeError):
             self._subject.dispatch_all(self._user_input)
 
-        self._controller4.run.assert_called_once_with(self._user_input)
+        self._error_raised_controller.run.assert_called_once_with(self._user_input)
