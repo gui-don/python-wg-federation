fe5bd0f20cb5f59ac700bb88898354bb27177ba7
Guillaume DONVAL <guillaume.donval@globaleagle.com>
2022-10-31 15:04:15 -0400

    feat: adds `data_transformation.loader` library

---
+++ src/wg_federation/data_transformation/loader/configuration_loader.py
@@ -0,0 +1,43 @@
+import logging
+
+from wg_federation.data_transformation.loader.configuration_loader_interface import ConfigurationLoaderInterface
+
+
+class ConfigurationLoader:
+    """
+    Read any configuration from any sources
+    """
+    _configuration_loaders: list[ConfigurationLoaderInterface] = None
+    _logger: logging.Logger = None
+
+    def __init__(
+            self,
+            configuration_loaders: list[ConfigurationLoaderInterface],
+            logger: logging.Logger,
+    ):
+        """
+        Constructor
+        :param configuration_loaders:
+        :param logger:
+        """
+        self._configuration_loaders = configuration_loaders
+        self._logger = logger
+
+    def load(self, source_kind: str, sources: tuple[str]):
+        """
+        Loads different configurations of source_kind from sources
+        :param source_kind: kind of sources
+        :param sources: sources
+        :return: raw configuration
+        """
+        for _configuration_loader in self._configuration_loaders:
+            if source_kind == _configuration_loader.get_supported_source():
+                self._logger.debug(
+                    f'{_configuration_loader.__class__} configuration loader supports {source_kind}. '
+                    f'Trying to load: {"; ".join(sources)}.'
+                )
+
+                return _configuration_loader.load_from_all(sources)
+
+        raise RuntimeError(f'Could not load any configuration from “{source_kind}”. '
+                           f'It seems not ConfigurationLoader supports this type of source.')
---
+++ src/wg_federation/data_transformation/loader/configuration_loader_interface.py
@@ -0,0 +1,32 @@
+from abc import ABC, abstractmethod
+
+
+class ConfigurationLoaderInterface(ABC):
+    """
+    Configuration Loader interface. Represents any configuration loader
+    """
+
+    @abstractmethod
+    def load_from_all(self, sources: tuple[str]) -> dict:
+        """
+        Look at all given sources and return an unified configuration.
+        :param sources: user inputs
+        :return: configuration
+        """
+
+    @abstractmethod
+    def load_from(self, source: str) -> dict:
+        """
+        Load configuration from the source.
+        :param source: source of configuration
+        :return: configuration
+        """
+
+    @staticmethod
+    @abstractmethod
+    def get_supported_source() -> str:
+        """
+        Whether the configuration loader supports the given source
+        :return: The kind of source this configuration loader supports.
+        """
+        return 'undefined'
